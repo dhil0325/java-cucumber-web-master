08aea86eb5a3529f116a90b5b0631a3d8dcbae47
regiewby <regiewby@gmail.com>
2022-12-23 19:23:57 +0700

    add keyword handler alert

--- src/main/java/app/bersama/Keyword.java
+++ src/main/java/app/bersama/Keyword.java
@@ -16,8 +16,14 @@ import java.time.Duration;
  */
 public class Keyword {
 
+    private static WebDriver webDriver;
+
+    public Keyword() {
+        webDriver = DriverManager.getInstance().getDriver();
+    }
+
     public static void navigateToUrl(String url) {
-        DriverManager.getInstance().getDriver().get(url);
+        webDriver.get(url);
     }
 
     public static void click(WebElement webElement) {
@@ -31,7 +37,7 @@ public class Keyword {
     }
 
     public static void validateElementIsVisibleAndEnabled(WebElement webElement) {
-        WebDriverWait wait = new WebDriverWait(DriverManager.getInstance().getDriver(), Duration.ofSeconds(5));
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOf(webElement));
 
         if (!webElement.isDisplayed() || !webElement.isEnabled()) {
@@ -41,30 +47,30 @@ public class Keyword {
     }
 
     public static void waitElementToBeDisplayed(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(DriverManager.getInstance().getDriver(), Duration.ofSeconds(25));
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(25));
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
     public static void takeScreenshot() {
         try {
-            TakesScreenshot screenshot = ((TakesScreenshot) DriverManager.getInstance().getDriver());
+            TakesScreenshot screenshot = ((TakesScreenshot) webDriver);
             File sourceFile = screenshot.getScreenshotAs(OutputType.FILE);
-            File destionationFile = new File("reports/screenshot");
+            File destinationFile = new File("reports/screenshot");
 
-            FileUtils.copyFile(sourceFile, destionationFile);
+            FileUtils.copyFile(sourceFile, destinationFile);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void assertCurrentUrl(String expectedUrl) {
-        String actualUrl = DriverManager.getInstance().getDriver().getCurrentUrl();
+        String actualUrl = webDriver.getCurrentUrl();
         Assert.assertEquals(actualUrl, expectedUrl);
     }
 
     public static void takeScreenshot(String fileName) {
         try {
-            TakesScreenshot screenshot = ((TakesScreenshot) DriverManager.getInstance().getDriver());
+            TakesScreenshot screenshot = ((TakesScreenshot) webDriver);
             File sourceFile = screenshot.getScreenshotAs(OutputType.FILE);
             File destinationFile = new File("reports/screenshots" + File.separator + fileName + "." + FileType.PNG.name());
 
@@ -72,5 +78,17 @@ public class Keyword {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void alertHandler(boolean isAccepted) {
+        if (isAccepted) {
+            webDriver.switchTo().alert().accept();
+        } else {
+            webDriver.switchTo().alert().dismiss();
+        }
+    }
+
+    public static String alertGetText() {
+        return webDriver.switchTo().alert().getText();
     }
 }
